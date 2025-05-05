33437f5819aba2e723113d71a652b75257712cca
H3lio2f <heliofragaofila.helio@gmail.com>
2025-05-05 16:07:35 +0100

    updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -46,7 +46,7 @@ export default function Home() {
  
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Next JS 14 Starter</h1>
+      <h1>Next JS 15 Starter</h1>
       <p>
         <Link className="text-indigo-600" href="/" title="inicio">
           inicio
