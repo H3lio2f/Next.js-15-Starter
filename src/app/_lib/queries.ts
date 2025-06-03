47ad86e65e020317a9325d399120eb801ebdc03f
H3lio2f <heliofragaofila.helio@gmail.com>
2025-06-03 15:09:44 +0100

    chore: update dependencies and remove TanStackProvider; refactor Providers component to directly use SessionProvider

--- src/app/_lib/queries.ts
+++
@@ -1,9 +0,0 @@
-import { useQuery } from "@tanstack/react-query";
-import { getUsers_action } from "./actions";
-
-export const useInfrographs = () => {
-  return useQuery({
-    queryKey: ["users"],
-    queryFn: async () => await getUsers_action(),
-  });
-};
--- src/providers/Providers.tsx
+++ src/providers/Providers.tsx
@@ -1,17 +1,14 @@
 "use client";
 
-import TanStackProvider from '@/providers/TanStackProvider';
 import { SessionProvider } from "next-auth/react";
 import { Toaster } from "react-hot-toast";
 
 export function Providers({ children }: { children: React.ReactNode }) {
 
   return (
-    <TanStackProvider>
-        <SessionProvider>
-            <Toaster position="top-center" reverseOrder={false} />
-            {children}
-        </SessionProvider>
-    </TanStackProvider>
+      <SessionProvider>
+          <Toaster position="top-center" reverseOrder={false} />
+          {children}
+      </SessionProvider>
   );
 }
